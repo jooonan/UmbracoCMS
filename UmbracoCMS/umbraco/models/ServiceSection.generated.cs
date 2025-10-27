325bd10276d22475f3c763ab0ed680517232e4b4
Johan An <johanan9606@gmail.com>
2025-10-27 21:29:30 +0100

    Service Details Added

--- UmbracoCMS/umbraco/models/ServiceSection.generated.cs
+++ UmbracoCMS/umbraco/models/ServiceSection.generated.cs
@@ -50,14 +50,6 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 		// properties
 
 		///<summary>
-		/// Item
-		///</summary>
-		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
-		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
-		[ImplementPropertyType("serviceItemList")]
-		public virtual global::Umbraco.Cms.Core.Models.Blocks.BlockListModel ServiceItemList => this.Value<global::Umbraco.Cms.Core.Models.Blocks.BlockListModel>(_publishedValueFallback, "serviceItemList");
-
-		///<summary>
 		/// Label
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
